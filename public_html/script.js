108c4e7b1d9ff5bfd3946af0a3457cdf59e31460
Joan <joanferns00@yahoo.co.uk>
2015-05-22 22:34:23 -0700

    Enter of the input field adds to the list. Made web service call to openweather. Added d3.js

--- public_html/script.js
+++ public_html/script.js
@@ -7,6 +7,19 @@
 
 
 var mist = angular.module("MainApp", ['ngRoute']);
+mist.directive('ngEnter', function(){
+    return function(scope, element, attrs){
+        element.bind("keydown press", function(event){
+            if(event.which === 13){
+                scope.$apply(function(){
+                    scope.$eval(attrs.ngEnter);
+                });
+//                event.preventDefault();
+            }
+            
+        });
+    };
+});
 mist.config(
     
 function($routeProvider, $locationProvider){
@@ -49,8 +62,18 @@ mist.controller("MCtrl", function($scope){
         $scope.taskadd = "";
         $scope.tasks  = ["Make bed", "Watch TV"];
         $scope.addItem = function(){
-            console.log("going to add to the list");
-            $scope.tasks.unshift($scope.taskadd);
+            
+            //if exists, dont add to the list.
+            var test = $scope.tasks;
+            
+            console.log(test.indexOf($scope.taskadd) === -1);
+            if(test.indexOf($scope.taskadd) === -1){
+                $scope.tasks.unshift($scope.taskadd);
+            }
+            else{
+                console.log("Item exists");
+                }            
+            
         };
         //Going to edit item
         $scope.editItem = function(){
@@ -58,26 +81,32 @@ mist.controller("MCtrl", function($scope){
         };
         
         $scope.isChecked = false;        
-        //Going to strike item
-        $scope.strikeItem = function(){
-            //strike out item 
-            $scope.isChecked  = !$scope.isChecked;
-//                $scope.strkStyle = {'text-decoration': 'line-through' };
-        };
+//        //Going to strike item
+//        $scope.strikeItem = function(){
+//            //strike out item 
+//            $scope.isChecked  = !$scope.isChecked;
+////                $scope.strkStyle = {'text-decoration': 'line-through' };
+//        };
         //Going to delete item
         $scope.delItem = function(item){
 //            $scope.tasks.indexOf(item);
             $scope.tasks.splice($scope.tasks.indexOf(item), 1);
         };
-
-        
-        
 });
 
-mist.controller("ACtrl", function($scope){
+mist.controller("ACtrl", function($scope, $http){
         // create a message to display in our view
         $scope.title = 'Weather';   
         $scope.message = 'Look up weater in your city';   
+        $http.get("http://api.openweathermap.org/data/2.5/weather?zip=94040,us")
+                .success(function(data, status, headers, config){
+                    console.log("Data from weather service");
+            console.log(data);
+            //place data in 
+                })
+                .error(function(data, status, headers, config){});
+        
+        
 });
 
 mist.controller("CCtrl", function($scope){
